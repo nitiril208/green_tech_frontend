e579eecaaec9c96b8721d07dc29dc004dc7676f3
kishanengineb <kishan@engineb.com>
2024-09-10 17:14:47 +0530

    some change

--- src/s3Client.ts
+++ src/s3Client.ts
@@ -1,5 +1,6 @@
 // src/s3Client.js
 import { S3Client } from "@aws-sdk/client-s3";
+console.log("VITE_AWS_REGION++++++", import.meta.env.VITE_AWS_REGION, "--->", import.meta.env.VITE_AWS_ACCESS_KEY_ID, "--->", import.meta.env.VITE_AWS_SECRET_ACCESS_KEY);
 
 const s3Client = new S3Client({
     region: import.meta.env.VITE_AWS_REGION,
